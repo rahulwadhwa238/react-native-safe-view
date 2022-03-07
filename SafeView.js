089cfe86871ba6d5914f93563586ab13a80d502d
Rahul Wadhwa <rahulwadhwa238@gmail.com>
2022-03-07 11:39:39 +0530

    Update SafeView.js

--- SafeView.js
+++ SafeView.js
@@ -2,15 +2,15 @@ import React from 'react'
 import {
     View, Platform, SafeAreaView, StatusBar
 } from 'react-native'
-export default function SafeView({ children, screenMode = 'safe' }) {
+export default function SafeView({ children, screenMode = 'full' | 'safe' | undefined }) {
     return (
-        screenMode != 'full' ?
+        screenMode == 'full' ?
+            <View style={{ flex: 1, flexGrow: 1, backgroundColor: '#FFF', }}>
+                {children}
+            </View>
+            :
             <SafeAreaView style={{ flex: 1, flexGrow: 1, paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0, backgroundColor: '#FFF' }}>
                 {children}
             </SafeAreaView>
-            :
-            <View style={{ flex: 1, flexGrow: 1, backgroundColor: '#FFF' }}>
-                {children}
-            </View>
     )
 }
